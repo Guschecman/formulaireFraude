4c7a193f5e8a8719702bed9eecc584d134668188
darde <darderomain16@gmail.com>
2026-05-27 09:58:39 +0200

    MAJ

--- src/Formulaire.java
+++ src/Formulaire.java
@@ -1,7 +1,20 @@
 import java.time.LocalDate;
 
 public class Formulaire {
+    private static int nbFormulaires = 0;
+
     private int id;
     private LocalDate dateCreation;
     private LocalDate dateModification;
+
+    public Formulaire() {
+        nbFormulaires+=1;
+        id = nbFormulaires;
+        dateCreation = LocalDate.now();
+        dateModification = LocalDate.now();
+    }
+
+    public int getId() {
+        return id;
+    }
 }
