b9f903eabbe0857ae606cbe76caa6ccdd742f6d8
augus <augustemanchec@gmail.com>
2026-05-27 10:03:53 +0200

    NEW MAJ

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LocalDate date = LocalDate.now();
+
+
+        Fraude f1 = new FraudePapier(date, "Antisèche trouvée", "Formules de maths", Type.PAPIER, "5cm", true);
+
+
+        ArrayList<Fraude> fraudes = new ArrayList<>();
+        fraudes.add(f1);
+
+
+            f1.afficher();
+        }
+    }
